1c364fb24c22aabc6ea1591041c29363a4135f2d
sharmili2203-ship-it <sharmili2203@gmail.com>
2025-09-17 17:21:31 +0530

    Add files via upload

---
+++ t1.py
@@ -0,0 +1,10 @@
+import turtle
+
+# Set up the screen size
+turtle.setup(800, 600)
+
+# Create the window/screen object correctly
+window = turtle.Screen()
+
+# Set the title of the window
+window.title('My First Turtle Graphics Program')
