f65990ae5d503ca4ed8d45916c518118c252edc5
sharmili2203-ship-it <sharmili2203@gmail.com>
2025-09-19 17:24:58 +0530

    Add files via upload

---
+++ superr.py
@@ -0,0 +1,27 @@
+class stud:
+    def __init__(self,a,b,c,):
+        self.sno=a
+        self.sname=b
+        self.sage=c
+    def disp(self):
+        print("no:",self.sno)
+        print("name:",self.sname)
+        print("age:",self.sage)
+
+
+
+class marks(stud):
+    def __init__(self,a,b,c,m1,m2,m3):
+        super().__init__ (a,b,c)
+        self.mark1=m1
+        self.mark2=m2
+        self.mark3=m3
+
+    def disp(self):
+        super().disp()
+        print("mark1:",self.mark1)
+        print("mark2:",self.mark2)
+        print("mark3:",self.mark3)
+
+ob=marks(1,'abc',16,17,18,19)
+ob.disp()
