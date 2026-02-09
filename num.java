42150550f6bf009b56dc8fce8eac3c22526b42f5
sharmili2203-ship-it <sharmili2203@gmail.com>
2026-02-09 18:02:34 +0530

    Add files via upload

---
+++ num.java
@@ -0,0 +1,21 @@
+import java . until scanner;
+class num{
+  public static void main(string args[])
+    Scanner sc=new Scanner(system.in);
+  int a;
+  a=Sc.next()
+    system.out(println("enter num:"+a):
+   if(a>0){
+    system.out(println("postive");
+               }
+    else if(a==0){
+     system.out(println("Zero");
+                }
+    else(a<0){
+      system.out println("negative");
+    }
+    }
+}
+           
+                   
+    
