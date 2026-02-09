dd01855b7133732d8de9b777c30ddbce8559c2fa
sharmili2203-ship-it <sharmili2203@gmail.com>
2026-02-09 18:03:54 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,10 @@
+import java.util.scanner;
+class reverse{
+  public static void main(String agrs[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number:");
+    int num=sc.nextInt();
+    int rev=0;
+    while(num!=0){
+      int digit 
+      
